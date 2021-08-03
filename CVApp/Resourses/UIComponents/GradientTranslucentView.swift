167e346d933263bd45c26b3a41c80d9a9971c8be
Agustin Errecalde <agustin.errecalde@brubank.com>
2021-08-03 03:28:26 -0300

    Pass gradient to components

---
+++ CVApp/Resourses/UIComponents/GradientTranslucentView.swift
@@ -0,0 +1,26 @@
+//
+//  GradientTranslucentView.swift
+//  CVApp
+//
+//  Created by Agustin Errecalde on 03/08/2021.
+//
+
+import SwiftUI
+
+struct GradientTranslucentView: View {
+    var body: some View {
+        Rectangle()
+            .fill(
+                LinearGradient(gradient: Gradient(stops: [
+                    .init(color: Color(UIColor.systemBackground).opacity(0.01), location: 0),
+                    .init(color: Color(UIColor.systemBackground), location: 1)
+                ]), startPoint: .top, endPoint: .bottom)
+            ).frame(height: 60)
+    }
+}
+
+struct GradientTranslucentView_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientTranslucentView()
+    }
+}
